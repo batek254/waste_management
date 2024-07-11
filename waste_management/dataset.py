8e6324133495de582cf162d2251ba340c5bc616f
Bartłomiej Fatyga <batek254@gmail.com>
2024-07-11 21:37:05 +0200

    adds data loading and saving in csv

--- waste_management/dataset.py
+++ waste_management/dataset.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+import os
 
 import typer
 from loguru import logger
 from tqdm import tqdm
+import pandas as pd
 
 from waste_management.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
@@ -11,18 +13,26 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
+    input_path: Path = RAW_DATA_DIR,
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+    garbage_types = os.listdir(input_path)
+    # Initialize an empty list to store image file paths and their respective labels
+    data = []
+
+    # Loop through each garbage type and collect its images' file paths
+    for garbage_type in tqdm(garbage_types, desc="Processing garbage types"):
+        logger.info(f"Processing {garbage_type}...")
+        for file in tqdm(os.listdir(os.path.join(input_path, garbage_type))):
+            # Append the image file path and its trash type (as a label) to the data list
+            data.append((os.path.join(input_path, garbage_type, file), garbage_type))
+    
+    # Convert the collected data into a DataFrame and save it as a CSV file
+    df = pd.DataFrame(data, columns=["filepath", "label"])
+    df.to_csv(output_path, index=False)
+
     logger.success("Processing dataset complete.")
-    # -----------------------------------------
 
 
 if __name__ == "__main__":
